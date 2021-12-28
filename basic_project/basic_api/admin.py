a19f23d3b613dc0a22ac47770bda3d6b6d55fde0
saulbenitez <saulbenitezg@outlook.com>
2021-12-28 14:14:37 -0600

    Models and Serializers statement

--- basic_project/basic_api/admin.py
+++ basic_project/basic_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from basic_api.models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    readonly_fields = ('date',)
+
+admin.site.register(Article, ArticleAdmin)
--- basic_project/basic_api/models.py
+++ basic_project/basic_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    ''' Class for users' published articles '''
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
